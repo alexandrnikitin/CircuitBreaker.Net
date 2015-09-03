7680793754b71020cb27172da9c2aeb563cf5561
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2015-09-03 17:02:48 +0300

    Small fixes

--- src/CircuitBreaker.Net/Config/CircuitBreakerConfig.cs
+++ src/CircuitBreaker.Net/Config/CircuitBreakerConfig.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace CircuitBreaker.Net.Config
 {
     public class CircuitBreakerConfig : ICircuitBreakerConfig
     {
         public TaskScheduler TaskScheduler { get; set; }
+        public TimeSpan Timeout { get; set; }
     }
 }
