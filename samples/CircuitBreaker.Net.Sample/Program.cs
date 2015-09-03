162c4ce2493f4595f905f88aa70a3812cf6b714c
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2015-09-03 15:42:55 +0300

    Add sample project

---
+++ samples/CircuitBreaker.Net.Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CircuitBreaker.Net.Sample
+{
+    public class ExternalService
+    {
+        public void Get()
+        {
+            throw new Exception();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var externalService = new ExternalService();
+            var circuitBreaker = new CircuitBreaker(TaskScheduler.Default);
+            circuitBreaker.Execute(externalService.Get);
+
+        }
+    }
+}
