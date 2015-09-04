66611a0a95e82860f430d1c06485b551ad9bf54c
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2015-09-04 10:12:37 +0300

    0.1.0-beta pushed :boom:

--- build/build.fsx
+++ build/build.fsx
@@ -9,7 +9,7 @@ let copyright = "Copyright © 2015"
 let authors = [ "Alexandr Nikitin" ]
 let company = "Alexandr Nikitin"
 let tags = ["CircuitBreaker"]
-let version = "0.0.1-beta"
+let version = "0.1.0-beta"
 
 let buildDir = "output"
 let packagingRoot = "./packaging/"
