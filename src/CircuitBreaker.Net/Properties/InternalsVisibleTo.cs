c3bef9d928ccaf4e90b791bb2d2c8284f4b025cf
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2015-09-04 13:12:54 +0300

    First test

---
+++ src/CircuitBreaker.Net/Properties/InternalsVisibleTo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CircuitBreaker.Net.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
---
+++ tests/CircuitBreaker.Net.Tests/States/ClosedCircuitBreakerStateTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using CircuitBreaker.Net.States;
+
+using NSubstitute;
+
+using Xunit;
+
+namespace CircuitBreaker.Net.Tests.States
+{
+    public class ClosedCircuitBreakerStateTests
+    {
+        private const int MaxFailures = 3;
+
+        private readonly TimeSpan Timeout = TimeSpan.FromMilliseconds(100);
+        private readonly ICircuitBreakerInvoker _invoker;
+        private readonly ClosedCircuitBreakerState _sut;
+        private readonly ICircuitBreakerSwitch _switch;
+
+        public ClosedCircuitBreakerStateTests()
+        {
+            _switch = Substitute.For<ICircuitBreakerSwitch>();
+            _invoker = Substitute.For<ICircuitBreakerInvoker>();
+            _sut = new ClosedCircuitBreakerState(_switch, _invoker, MaxFailures, Timeout);
+        }
+
+        public class InvocationFailsTests : ClosedCircuitBreakerStateTests
+        {
+            [Fact]
+            public void OpensCircuitAfterMaxFailures()
+            {
+                _sut.InvocationFails();
+                _switch.DidNotReceive().OpenCircuit(Arg.Any<ICircuitBreakerState>());
+                _sut.InvocationFails();
+                _sut.InvocationFails();
+                _switch.Received().OpenCircuit(Arg.Is(_sut));
+            }
+        }
+    }
+}
