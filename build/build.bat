d0bdb04c64393d0a1e162bc065fd36647cf09a6e
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2015-09-04 09:57:06 +0300

    Target and build mode in build script

--- build/build.bat
+++ build/build.bat
@@ -2,5 +2,12 @@
 cls
 set encoding=utf-8
 ..\.nuget\NuGet.exe restore ..\.nuget\packages.config -PackagesDirectory ..\packages
-"..\packages\FAKE.4.3.4\tools\Fake.exe" build.fsx
+
+SET TARGET="Default"
+IF NOT [%1]==[] (set TARGET="%1")
+
+SET BUILDMODE="Release"
+IF NOT [%2]==[] (set BUILDMODE="%2")
+
+"..\packages\FAKE.4.3.4\tools\Fake.exe" build.fsx "target=%TARGET%" "buildMode=%BUILDMODE%"
 pause
