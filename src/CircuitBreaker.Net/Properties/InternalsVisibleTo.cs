4e8c7190c00c1b2eebcfb34dc80e9ff74ab11269
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2016-11-14 20:55:58 +0100

    Gave a strong name to CircuitBreaker.Net assembly

--- src/CircuitBreaker.Net/Properties/InternalsVisibleTo.cs
+++ src/CircuitBreaker.Net/Properties/InternalsVisibleTo.cs
@@ -1,4 +1,14 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("CircuitBreaker.Net.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("CircuitBreaker.Net.Tests,PublicKey=" +
+  "0024000004800000940000000602000000240000525341310004000001000100cf7e8e414bd858"+
+  "960674b2000ef2df586f00bee296a55e5cb0c8771621a22fde73e8a0c50b66cd35b52496af6661"+
+  "b8d87266eafc493c057b19b2879cc2dab65a72318917b2537cd0a1acd0a01e15727b1ce3855285"+
+  "8edc86f2114e0cf15f73c4a49dc04f53d272fe8a9b8d0a0a16a7b4107e3f43035335b3391b5f62"+
+  "3714669f")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=" +
+  "0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99" +
+  "c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654" +
+  "753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46" +
+  "ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484c" +
+  "f7045cc7")]
