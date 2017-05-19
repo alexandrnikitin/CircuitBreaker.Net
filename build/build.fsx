0d4595a89be9afae0e34812f8d59250f07e5c2ca
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-05-19 17:28:57 +0300

    Bump

--- build/build.fsx
+++ build/build.fsx
@@ -9,7 +9,7 @@ let copyright = @"Copyright 2015"
 let authors = [ "Alexandr Nikitin" ]
 let company = "Alexandr Nikitin"
 let tags = ["CircuitBreaker"; "Circuit"; "Breaker"; "C#"]
-let version = "0.5.0"
+let version = "0.6.0"
 
 let buildDir = "output"
 let buildTestsDir = "output-tests"
